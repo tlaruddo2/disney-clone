7748ff530b96807ed845cb1d997fc93c6066801d
kukjinkim <tlaruddo2@gmail.com>
2023-08-18 19:08:39 -0700

    feat: set up firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const firebase = require("firebase");
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider(); // social google login
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
