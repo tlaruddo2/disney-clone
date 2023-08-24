1ecd6c9289cc90087aae45ea55e764875f93f6de
kukjinkim <tlaruddo2@gmail.com>
2023-08-23 22:10:20 -0700

    feat: add video to viewer

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -5,18 +5,36 @@ const Viewers = (props) => {
     <Container>
       <Wrap>
         <img src="/images/viewers-disney.png" alt="" />
+        <video loop autoPlay>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-marvel.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-national.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-pixar.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-starwars.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+        </video>
       </Wrap>
     </Container>
   );
@@ -33,14 +51,6 @@ const Container = styled.div`
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
   }
-
-  /* display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 100%;
-  height: 300px;
-  margin: 40px;
-  padding: 0 20px; */
 `;
 
 const Wrap = styled.div`
@@ -65,6 +75,26 @@ const Wrap = styled.div`
     width: 100%;
     z-index: 1;
     top: 0;
+  }
+
+  video {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    opacity: 0;
+    z-index: 0;
+  }
+
+  &:hover {
+    box-shadow: rgb(0 0 0 / 0.8) 0px 40px 58px -16px,
+      rgb(0 0 0 / 0.72) 0px 30px 220px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+
+    video {
+      opacity: 1;
+    }
   }
 `;
 
